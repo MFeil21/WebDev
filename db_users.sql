a70275bec6696ccb16cbfe6688971ae6cac16610
Manuel Wetzl <Manuel.Wetzl@Hof-university.de>
2022-05-11 18:00:32 +0200

    Grundeinrichtung mit DB Anbindung

---
+++ db_users.sql
@@ -0,0 +1,53 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: db_users
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `db_users`
+--
+
+DROP TABLE IF EXISTS `db_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `db_users` (
+  `ID` int(5) NOT NULL,
+  `pseudonym` varchar(25) NOT NULL,
+  `vorname` varchar(25) NOT NULL,
+  `nachname` varchar(25) NOT NULL,
+  `geburtsjahr` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `db_users`
+--
+
+LOCK TABLES `db_users` WRITE;
+/*!40000 ALTER TABLE `db_users` DISABLE KEYS */;
+INSERT INTO `db_users` VALUES (0,'hans','','',0),(0,'hans','','',0),(0,'hans','','',0),(0,'hans','','',0),(0,'testhansel','','',0),(0,'testhansel','','',0);
+/*!40000 ALTER TABLE `db_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-11 17:56:09
--- readme.txt
+++ readme.txt
@@ -4,3 +4,22 @@ Matthias Check
 Sabrina Check
 Manuel Check
 final Check
+///
+//
+/
+//
+///
+Es muessen die Dateien main.html, main.php, poll.html, result.html, script.js, style.css nach C:\xampp\htdocs\Testat02 kopiert werden.
+Aktuell ist nur in main.html&php relevanter Content
+Zuseatzlich wird die Datenbank benoetigt.
+DB-Import:
+Datei "db_users.sql" nach C:\xampp\mysql\bin kopieren
+CMD öffnen:
+cd C:\xampp\mysql\bin
+mysql -u root -p db_users < db_users.sql
+//
+DB-Export auf der CMD:
+cd C:\xampp\mysql\bin
+mysqldump -u root -p db_users > db_users.sql
+(Passwortabfrage mit Enter bestätigen)
+Datei liegt anschließend unter C:\xampp\mysql\bin\db_users.sql
