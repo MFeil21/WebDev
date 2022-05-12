8987018da88db59362b6ecc33e3bf2371dccaee6
Manuel Wetzl <Manuel.Wetzl@Hof-university.de>
2022-05-12 22:01:06 +0200

    Eingabe -> DB erfolgreich. Ausgabe Stimmanzahl aus DB in Arbeit

--- result.php
+++ result.php
@@ -9,6 +9,27 @@ if ($verbindung -> connect_error) {
     die ("Verbindung fehlgeschlagen: " . $verbindung -> connect_error);
 }
 
+$tablename = "db_kandidaten";
+$columns = ['id', 'vorname', 'nachname', 'stimmen'];
+function lese_daten($verbindung, $tablename, $columens){
+  $columnName = implode(", ", $columns);
+  $dbabfrage = "SELECT ".$columnName." FROM $tablename"." ORDER BY id DESC";
+  $result = $verbindung->query($dbabfrage);
+}
+if($result== true){ 
+  if ($result->num_rows > 0) {
+     $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+     $msg= $row;
+  } else {
+     $msg= "Keine Daten vorhanden"; 
+  }
+ }
+ else{
+   $msg= mysqli_error($verbindung);
+ }
+ return $msg;
+
+/*
 $id = $_POST['id'];
 
 $sqldbcommand = "UPDATE db_kandidaten SET stimmen = stimmen + 1 WHERE id = '$id'";
@@ -20,6 +41,6 @@ if ($verbindung->query($sqldbcommand) === TRUE) {
     echo "Fehler: " . $sqldbcommand . "<br>" . $verbindung->error;
   }
 
-header( 'Location: result.html' );
+header( 'Location: result.html' );*/
 
 ?>
