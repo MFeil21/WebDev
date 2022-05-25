576dfdb06da405f4b12d11ba2b65e40a6a3a9f72
Uni <Uni@PCMAT01.win.feil.me>
2022-05-25 23:50:04 +0200

    +added Diagramme Signed-off-by: Uni <Uni@PCMAT01.win.feil.me>

--- script.js
+++ script.js
@@ -5,11 +5,10 @@
 		summeStimmen = Anzahl aller abgegebener Stimmen
 		summeErgebnis = Anzahl aller gültiger Stimmen nach Anwendung der 5% Hürde
 	*/
-
-	//Abfrage der Stimmen
-	var stimmen = [] //SQL Abfrage hier einfügen
-	var ergebnis = stimmen;
 	
+	//Abfrage der Stimmen
+	var stimmen = [100, 200, 400, 50, 100, 200]; //SQL Abfrage hier einfügen
+	var ergebnis = [...stimmen];
 	//Setze alle Stimmen unter der 5% Hürde auf 0 und ermittle den Divisor
 	var hürde = 0.05
 	var summeStimmen = 0
@@ -72,7 +71,6 @@
 			break
 		}
 	}
-	console.log(ergebnis)
 
 	//Poll Bestätigung nur möglich wenn alle Felder ausgefüllt wurden. 
 	function stimmeAbgeben() {
@@ -80,3 +78,42 @@
 			window.location.href = 'result.html'
 		} else alert("Bitte alle Pflichtfelder ausfüllen und ihrer Demokratischen Pflicht nachkommen indem Sie eine Partei wählen.")
 	}
+
+	//Diagramme Klasse Partei
+	const partei = [
+		{name: "DPP", wähler: stimmen[0], mandate: ergebnis[0], farbe : "#3366ff"},
+		{name: "MUT", wähler: stimmen[1], mandate: ergebnis[1], farbe : "#00cc66"},
+		{name: "NERD", wähler: stimmen[2], mandate: ergebnis[2], farbe : "#9933ff"},
+		{name: "LUST", wähler: stimmen[3], mandate: ergebnis[3], farbe : "#ff0000"},
+		{name: "FFF", wähler: stimmen[4], mandate: ergebnis[4], farbe : "#ffff00"},
+		{name: "fNEP", wähler: stimmen[5], mandate: ergebnis[5], farbe : "#33cccc"}
+	];
+	if(übrig !== 0){
+		partei.pop({name: "Losentscheid", mandate: übrig, farbe: "#000000"});
+	}
+
+	//Donut Diagramm
+	let ctx = document.getElementById("donut").getContext("2d");
+    let winkel = Math.PI;
+    for (let party of partei) {
+        let anteil = (party.mandate/(sitze*2)) * 2 * Math.PI;
+        ctx.beginPath();
+        ctx.arc(100, 100, 100, winkel, winkel + anteil);
+        winkel += anteil;
+        ctx.lineTo(100, 100);
+        ctx.fillStyle = party.farbe;
+        ctx.fill();
+    }
+	ctx.beginPath();
+	ctx.arc(100, 100, 40, Math.PI, 0);
+	ctx.lineTo(100, 100);
+	ctx.fillStyle = "#ffffff";
+    ctx.fill();
+
+	//Balken Diagramm
+	let ctx2 = document.getElementById("balken").getContext("2d");
+	let balken = 0;
+	for (let party of partei) {
+		ctx2.fillStyle = party.farbe;
+        ctx2.fillRect(balken+=40, 100, 20, (-party.wähler/summeStimmen)*200 );
+    }
