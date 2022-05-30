a6a84dad98708a72b9d196d08c9ec4950fb6a8f5
Manuel Wetzl <Manuel.Wetzl@Hof-university.de>
2022-05-30 17:22:44 +0200

    result.php sendet id, stimmen an $json array

--- script.js
+++ script.js
@@ -73,12 +73,12 @@
 	}
 
 	//Poll Bestätigung nur möglich wenn alle Felder ausgefüllt wurden. 
-	function stimmeAbgeben() {
-			/*urspruenglicher Code: (Es sind kein Buttons vorhanden, function liefert permannt Fehler)
+	/*function stimmeAbgeben() {
+			
 			if(document.getElementById("Vorname").value.length !== 0 && document.getElementById("Nachname").value.length !== 0 && document.getElementById("Geburtsdatum").value.length !== 0 && document.getElementById("Studiengang").value.length !== 0 && (b1.checked || b2.checked || b3.checked || b4.checked || b5.checked || b6.checked)){
-				window.location.href = 'result.html'*/
+				window.location.href = 'result.html'
 		} else alert("Bitte alle Pflichtfelder ausfüllen und ihrer Demokratischen Pflicht nachkommen indem Sie eine Partei wählen.")
-	}
+	}*/
 	//Diagramme Klasse Partei
 	const partei = [
 		{name: "DPP", wähler: stimmen[0], mandate: ergebnis[0], farbe : "#3366ff"},
